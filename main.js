3b44a567159c3a9baa59be309ac8584c2be10180
MassiBaldi <massibaldi85@gmail.com>
2019-01-10 16:35:03 +0100

    inizio mile2

--- main.js
+++ main.js
@@ -8,111 +8,149 @@ $(document).ready(function(){
     success: function(data){
       console.log(data);
 
-      var objSospFat = {
-        January: 0,
-        February: 0,
-        March: 0,
-        April: 0,
-        May: 0,
-        June: 0,
-        July: 0,
-        August: 0,
-        September: 0,
-        October: 0,
-        November: 0,
-        December: 0
-      };
+      graficoLinea(data);
 
-      for (var i = 0; i < data.length; i++) {
-          var obj = data[i];
-          //console.log(obj);
-          var giornoMeseAnno = obj.date;
-          //console.log(giornoMeseAnno);
-          var meseNumero = moment(giornoMeseAnno, 'DD/MM/YYYY').format('MMMM');
-          //console.log(mese);
+      graficoTorta(data);
 
-          objSospFat[meseNumero] += obj.amount;
-      }
-      //console.log(objSospFat);
-
-      var arrayLabelsFat = [];
-      var arrayDataFat = [];
-
-      for (var key in objSospFat) {
-        arrayLabelsFat.push(key);
-        arrayDataFat.push(objSospFat[key]);
-      }
-
-      console.log(arrayLabelsFat);
-      console.log(arrayDataFat);
-
-      var ctxLine = document.getElementById('myChartLine');
-      var chart = new Chart(ctxLine, {
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: {
-            labels: arrayLabelsFat,
-            datasets: [{
-                label: "Period 1",
-                // backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: arrayDataFat,
-            }]
-        },
-      });
-
-      var objSospVend = {};
-      var venditeTotale = 0;
-
-      for (var i = 0; i < data.length; i++) {
-          var obj = data[i];
-          //console.log(obj);
-          var nome = obj.salesman;
-          //console.log(nome);
-
-          if (objSospVend[nome] == undefined)
-          {
-              objSospVend[nome] = 0;
-          }
-
-          objSospVend[nome] += obj.amount;
-          venditeTotale += obj.amount;
-      }
-      //console.log(objSospVend);
-
-      var arrayLabelsVend = [];
-      var arrayDataVend = [];
-
-      for (var key in objSospVend) {
-        var percentualeVendite = objSospVend[key] / venditeTotale * 100;
-        arrayLabelsVend.push(key);
-        arrayDataVend.push(percentualeVendite.toFixed(2));
-      }
-
-      console.log(arrayLabelsVend);
-      console.log(arrayDataVend);
-
-
-      var ctx = document.getElementById('myChartPie');
-      var myPieChart = new Chart(ctx,{
-        type: 'pie',
-        data: {
-            datasets: [{
-                data: arrayDataVend,
-                backgroundColor: ['green', 'blue', 'orange', 'red']
-
-            }],
-
-            labels: arrayLabelsVend,
-
-        }
-      });
     },
     error: function() {
       alert('errore')
     }
   });
 
+  $('.btn').click(function(){
+    // alert('ciao');
+    var valoreAggiunto = parseInt($('input').val());
+    //console.log(valoreAggiunto);
+    var mesiSelect = $('.mesi').val();
+    //console.log(mesiSelect);
+    var venditoriSelect = $('.venditori').val();
+    //console.log(venditoriSelect);
+
+    // $.ajax({
+    //   url: urlApi,
+    //   method: 'PUT',
+    //   success: function(data) {
+    //     graficoLinea(data);
+    //
+    //     graficoTorta(data);
+    //
+    //   },
+    //   error: function() {
+    //     alert('errore')
+    //   }
+    // });
+  })
+
+
 });
+
+//funzione per grafico linea
+function graficoLinea(data){
+  var objSospFat = {
+    January: 0,
+    February: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    August: 0,
+    September: 0,
+    October: 0,
+    November: 0,
+    December: 0
+  };
+
+  for (var i = 0; i < data.length; i++) {
+      var obj = data[i];
+      //console.log(obj);
+      var giornoMeseAnno = obj.date;
+      //console.log(giornoMeseAnno);
+      var meseNumero = moment(giornoMeseAnno, 'DD/MM/YYYY').format('MMMM');
+      //console.log(meseNumero);
+
+      objSospFat[meseNumero] += obj.amount;
+  }
+  //console.log(objSospFat);
+
+  var arrayLabelsFat = [];
+  var arrayDataFat = [];
+
+  for (var key in objSospFat) {
+    arrayLabelsFat.push(key);
+    arrayDataFat.push(objSospFat[key]);
+    $('.mesi').append('<option>' + key + '</option>');
+  }
+
+  console.log(arrayLabelsFat);
+  console.log(arrayDataFat);
+
+  var ctxLine = document.getElementById('myChartLine');
+  var chart = new Chart(ctxLine, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: arrayLabelsFat,
+        datasets: [{
+            label: "Period 1",
+            //backgroundColor: 'rgb(255, 255, 255)',
+            borderColor: 'rgb(0, 0, 0)',
+            data: arrayDataFat,
+        }]
+    },
+  });
+}
+
+//funzione grafico torta
+function graficoTorta(data) {
+  var objSospVend = {};
+  var venditeTotale = 0;
+
+  for (var i = 0; i < data.length; i++) {
+      var obj = data[i];
+      //console.log(obj);
+      var nome = obj.salesman;
+      //console.log(nome);
+
+      if (objSospVend[nome] == undefined)
+      {
+          objSospVend[nome] = 0;
+      }
+
+      objSospVend[nome] += obj.amount;
+      venditeTotale += obj.amount;
+  }
+  //console.log(objSospVend);
+
+  var arrayLabelsVend = [];
+  var arrayDataVend = [];
+
+  for (var key in objSospVend) {
+    var percentualeVendite = objSospVend[key] / venditeTotale * 100;
+    arrayLabelsVend.push(key);
+    arrayDataVend.push(percentualeVendite.toFixed(2));
+    $('.venditori').append('<option>' + key + '</option>');
+  }
+
+  console.log(arrayLabelsVend);
+  console.log(arrayDataVend);
+
+
+  var ctx = document.getElementById('myChartPie');
+  var myPieChart = new Chart(ctx,{
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: arrayDataVend,
+            backgroundColor: ['green', 'blue', 'orange', 'red']
+
+        }],
+
+        labels: arrayLabelsVend,
+
+    }
+  });
+}
